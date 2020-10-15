23d71470ed15502b100bf33b5a4ae56fabd4690a
Ryogahp <72944317+Ryogahp@users.noreply.github.com>
2020-10-15 16:32:13 -0400

    Add files via upload

---
+++ precache-manifest.52019bafb6f804950fa4c34c2e97bc66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c9223d5d0ae06ac523f03bd41df7d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd1fa8366b249626b760",
+    "url": "/static/css/2.121aab0a.chunk.css"
+  },
+  {
+    "revision": "8a3b09686779099f03ee",
+    "url": "/static/css/main.503515c7.chunk.css"
+  },
+  {
+    "revision": "dd1fa8366b249626b760",
+    "url": "/static/js/2.3ab6f33e.chunk.js"
+  },
+  {
+    "revision": "320c461343b3b6f4581eeb2b7ffe197f",
+    "url": "/static/js/2.3ab6f33e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a3b09686779099f03ee",
+    "url": "/static/js/main.02a8048b.chunk.js"
+  },
+  {
+    "revision": "8fea6d0a4fa9cd9801da",
+    "url": "/static/js/runtime-main.2eb52750.js"
+  }
+]);
